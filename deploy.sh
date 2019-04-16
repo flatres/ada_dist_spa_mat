b443b9a6f3e0968212fe379de3e40d80b2d7d1c3
SD Flatres <flatres@gmail.com>
2019-04-16 12:29:30 +0100

    SPA Build  on 2019-04-16 12:29:30

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,4 @@ echo "Updating Composer"
 cd src/api
 composer update
 echo "Syncing"
-sudo rsync -av --delete-after /var/www/build/source/ /var/www/html/ --exclude '.git' --exclude '.htaccess'
-echo "Setting Environment"
-cp /var/www/build/env/prod.env /var/www/html/.env
-
+sudo rsync -av --delete-after /var/www/build/src/ /var/www/html/ --exclude '.git' --exclude '.htaccess'
