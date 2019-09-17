a636e2088af18804a7bf1b31f1d30e015ac45422
Simon Flatres <flatres@gmail.com>
2019-09-17 15:29:05 +0100

    Filestore Permissions

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ cd src/api
 composer update
 echo "Syncing"
 sudo rsync -av --delete-after /var/www/build/src/ /var/www/html/ --exclude '.git' --exclude '.htaccess' --exclude 'api/v1/public/filestore/*'
+chmod -R 777 /var/www/html/api/v1/public/filestore
