89a57a56a47303782aab42350bfed8dff8e05386
SD Flatres <flatres@gmail.com>
2019-10-07 11:46:50 +0000

    RSYNC Exclude

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo "Updating Composer"
 cd src/api
 composer update
 echo "Syncing"
-sudo rsync -av --delete-after /var/www/build/src/ /var/www/html/ --exclude '.git' --exclude '.htaccess' --exclude 'api/v1/public/filestore/*'
+sudo rsync -av --delete-after /var/www/build/src/ /var/www/html/ --exclude '.git' --exclude '.htaccess' --exclude 'api/v1/public/filestore'
 chmod -R 777 /var/www/html/api/v1/public/filestore
