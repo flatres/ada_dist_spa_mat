7bd5b90368ce96de2d610c64f39a5d969d7fc8eb
SD Flatres <flatres@gmail.com>
2020-01-22 09:19:04 +0000

    SPA Build  on 2020-01-22 09:19:04

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ composer update
 echo "Syncing"
 sudo rsync -av --delete-after /var/www/build/src/ /var/www/html/ --exclude 'portal' --exclude 'igc' --exclude '.git' --exclude '.htaccess' --exclude 'api/v1/public/filestore'
 sudo chmod -R 777 /var/www/html/api/v1/public/filestore
+sudo chmod -R 777 /var/www/html/api/v1/logs
