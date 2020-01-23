7cb8d4d1e02af2168b8d6c34c7869d0935f4e842
SD Flatres <flatres@gmail.com>
2020-01-23 13:20:05 +0000

    SPA Build  on 2020-01-23 13:20:05

--- src/api/v1/dependencies/Databases/ISams.php
+++ src/api/v1/dependencies/Databases/ISams.php
@@ -1,25 +1,22 @@
 <?php
 namespace Dependency\Databases;
 
+define('ISAMS_IP', getenv("ISAMS_IP"));
+define('ISAMS_DB', getenv("ISAMS_DB"));
+define('ISAMS_USER', getenv("ISAMS_USER"));
+define('ISAMS_PWD', getenv("ISAMS_PWD"));
 
 class ISams extends \Dependency\MSSql
 {
-
   public function __construct() {
 
-    // $ip = "192.168.2.165";
     $ip="192.168.2.164";
     $db = "iSAMS";
-    //$database = "dbo";
     $user = "mcnode";
     $pwd = "MM0ndcol1s.03";
-
     $this->connect($ip, $db,  $user, $pwd);
 
   }
-
-
-
 }
 
  ?>
