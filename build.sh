4c4cb0e3030f1afa61d8844a186893241e300bcb
SD Flatres <flatres@gmail.com>
2020-01-23 08:28:02 +0000

    SPA Build  on 2020-01-23 08:28:02

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ quasar build
 echo "Syncing With Quasar Build Files"
 rsync -avr --delete-after ../../dist/spa/ src/ --exclude '.git' --exclude 'api' --exclude '.htaccess'
 echo "Syncing API"
-rsync -avr --delete-after ../../api/ src/api/ --exclude 'api'
+rsync -avr --delete-after ../../api/ src/api/ --exclude 'api' --exclude '../../api/v1/logs'
 echo "Commiting and Pushing"
 cd spa
 git add --all
