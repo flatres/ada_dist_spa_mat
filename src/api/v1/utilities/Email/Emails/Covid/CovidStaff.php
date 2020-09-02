50da659f413b3d7eb70531920f27ed2cb9ff2630
SD Flatres <flatres@gmail.com>
2020-09-02 15:37:00 +0100

    SPA Build  on 2020-09-02 15:37:00

--- src/api/v1/utilities/Email/Emails/Covid/CovidStaff.php
+++ src/api/v1/utilities/Email/Emails/Covid/CovidStaff.php
@@ -9,7 +9,7 @@ class CovidStaff {
       $to = 'sdf@marlboroughcollege.org';
       $email = new \Utilities\Email\Email($to, "MC Covid-19 Check In");
 
-      $url = "http://".$_SERVER['SERVER_NAME']."/aux/covid/staff?h=" . $hash;
+      $url = "https://ada.marlboroughcollege.org/aux/covid/staff?h=" . $hash;
 
       $content = $email->template('covid.staff', array("name"=>$name, "action_url" => $url));
 
--- src/api/v1/utilities/Email/Emails/Covid/CovidStudents.php
+++ src/api/v1/utilities/Email/Emails/Covid/CovidStudents.php
@@ -9,7 +9,7 @@ class CovidStudents {
       $to = 'sdf@marlboroughcollege.org';
       $email = new \Utilities\Email\Email($to, "MC Covid-19 Check In");
 
-      $url = "http://".$_SERVER['SERVER_NAME']."/aux/covid/students?h=" . $hash;
+      $url = "https://ada.marlboroughcollege.org/aux/covid/students?h=" . $hash;
 
       $content = $email->template('covid.students', array("name"=>$name, "action_url" => $url));
 
