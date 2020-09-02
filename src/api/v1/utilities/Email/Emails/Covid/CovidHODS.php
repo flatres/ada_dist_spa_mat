bbe58f5d596e8f2c80165588d5ab72759a991b92
SD Flatres <flatres@gmail.com>
2020-09-02 16:11:42 +0100

    SPA Build  on 2020-09-02 16:11:42

--- src/api/v1/utilities/Email/Emails/Covid/CovidHODS.php
+++ src/api/v1/utilities/Email/Emails/Covid/CovidHODS.php
@@ -8,7 +8,7 @@ class CovidHODS {
 			//to, subject, tag, track
       $to = 'sdf@marlboroughcollege.org';
 			$prefix = 'All Clear:';
-			$msg = 'All clear. No members of staff have reported Covid-19 symptoms today';
+			$msg = 'All clear. No members of staff have reported Covid-19 symptoms today.';
 
 			$alertHTML = '';
 			$notAnsweredHTML = '';
@@ -17,7 +17,7 @@ class CovidHODS {
 			if (count($notAnsweredNames) > 0) {
 				$prefix ='Attention: ';
 				$msg = 'All clear so far but the following members of staff have not checked in today.';
-				$notAnsweredHTML = "<p style='margin-top:0; margin-bottom: 10px; color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:bold;' >Not Checked In</p>";
+				$notAnsweredHTML = "<p style='margin-top:0; margin-bottom: 5px; margin-left: 5px; color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:bold;' >Not Checked In</p>";
 				foreach($notAnsweredNames as $n) $notAnsweredHTML .= "<p style='margin-top:0;color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:normal;' >$n</p>";
 			}
 
@@ -25,12 +25,12 @@ class CovidHODS {
 				$prefix = "!!! Alert !!! ";
 				$msg = 'The following member(s) of staff have reported possible Covid-19 symptoms.';
 				$alertHTML = '';
-				foreach($alertNames as $n) $alertHTML .= "<p style='margin-top:0;color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:normal;' >$n</p>";
+				foreach($alertNames as $n) $alertHTML .= "<p style='margin-top:0;color:#8b0000; margin-left: 5px; font-size:16px;line-height:1.5em;text-align:left;font-weight:bold;' >$n</p>";
 			}
 
 			if (count($notInWorkNames) > 0) {
-				$notInWorkHTML = "<p style='margin-top:0; margin-bottom: 10px; color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:bold;' >Not In School Today</p>";
-				foreach($notInWork as $n) $notInWorkHTML .= "<p style='margin-top:0;color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:normal;' >$n</p>";
+				$notInWorkHTML = "<p style='margin-top:0; margin-bottom: 5px; color:#2F3133;font-size:16px;line-height:1.5em;text-align:left;font-weight:bold;' >Not In School Today</p>";
+				foreach($notInWork as $n) $notInWorkHTML .= "<p style='margin-top:0;color:#2F3133;margin-left: 5px;font-size:16px;line-height:1.5em;text-align:left;font-weight:normal;' >$n</p>";
 			}
 
       $email = new \Utilities\Email\Email($to, "$prefix HOD Covid-19 Check-In Summary");
